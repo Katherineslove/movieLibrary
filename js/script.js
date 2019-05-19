b02c93c8dc25b148d0636ac9590844c4ac0d3628
Katherine Love <270010932@bun2-206-14.local>
2019-05-20 08:48:02 +1200

    Added Movies

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var movies = [
     directors: ["Ron Clements, John Musker"],
     bio: "Ladyfish tries to live on land",
     genre: ["Animation", "Musical", "Family", "Fantasy", "Romance"],
-    length: 85,
+    movieLength: 85,
     poster: "mermaid.jpg"
   }, //The Little Mermaid
 
@@ -17,7 +17,7 @@ var movies = [
     directors: ["Chris Buck, Kevin Lima"],
     bio: "A monkey man",
     genre: ["Adventure", "Musical", "Family", "Comedy"],
-    length: 88,
+    movieLength: 88,
     poster: "tarzan.jpeg"
   }, //Tarzan
 
@@ -28,7 +28,7 @@ var movies = [
     directors: ["Andrew Stanton"],
     bio: "A clown fish that has lost his son",
     genre: ["Animation", "Adventure", "Comedy", "Family"],
-    length: 104,
+    movieLength: 104,
     poster: "dory.jpg"
   }, //Finding Dory
 
@@ -39,7 +39,7 @@ var movies = [
     directors: ["Brad Bird"],
     bio: "A talking rat that can cook",
     genre: ["Animation", "Family", "Fantasy", "Drama", "Coming of age"],
-    length: 111,
+    movieLength: 111,
     poster: "rat.jpg"
   }, //Ratatouille
 
@@ -50,7 +50,7 @@ var movies = [
     directors: ["Bryon Howard, Nathan Greno"],
     bio: "A chic with really long hair",
     genre: ["Animation", "Musical", "Family", "Fantasy", "Romance"],
-    length: 100,
+    movieLength: 100,
     poster: "tangled.jpg"
   }, //Tangled
 
@@ -61,7 +61,7 @@ var movies = [
     directors: ["Robert Stromberg"],
     bio: "A mythical creature",
     genre: ["Action", "Aventure", "Family", "Fantasy", "Romance"],
-    length: 97,
+    movieLength: 97,
     poster: "maleficent.jpg"
   }, //Maleficent
 
@@ -72,7 +72,7 @@ var movies = [
     directors: ["Pete Docter"],
     bio: "Bunch of emotions",
     genre: ["Animation", "Adventure", "Family", 'Fantasy', "Drama"],
-    length: 102,
+    movieLength: 102,
     poster: "inside-out.jpg"
   }, //Inside Out
 
@@ -83,7 +83,7 @@ var movies = [
     directors: ["Ron Clements, John Musker"],
     bio: "Young girl ventures with demi-god",
     genre: ["Animation", "Adventure", "Musical", "Family", "Fantasy"],
-    length: 113,
+    movieLength: 113,
     poster: "moana.jpg"
   }, //Moana
 
@@ -94,9 +94,31 @@ var movies = [
     directors: ["Brad Bird"],
     bio: "A family with a psycho baby",
     genre: ["Animation", "Comedy", "Family", "Superhero", "Science fiction"],
-    length: 125,
+    movieLength: 125,
     poster: "incredibles.jpeg"
-  } //Incredibles2
+  }, //Incredibles2
+
+  {
+    id: 10,
+    title: "A Wrinkle In Time",
+    year: 2018,
+    directors: ["Ava DuVernay"],
+    bio: "Father disappears and girl goes to find him",
+    genre: ["Adventure", "Fantasy", "Family", "Science fiction"],
+    movieLength: 110,
+    poster: "wrinkle.jpg"
+  }, //Wrinkle In Time
+
+  {
+    id: 11,
+    title: "Christopher Robin",
+    year: 2018,
+    directors: ["Marc Forster"],
+    bio: "The father of the original CR",
+    genre: ["Animation", "Comedy", "Family", "Musical"],
+    movieLength: 103,
+    poster: "robin.jpeg"
+  } //Christopher Robin
 ]
 
 // console.log(movies);
@@ -110,24 +132,23 @@ for (var i = 0; i < movies.length; i++) {
 
 // WAY NUMBER TWO:
 
-  var genreClass = '';
-  if (movie.genre[0] === 'Animation') {
-    genreClass = 'border-primary';
-
-  } else if (movie.genre[0] === 'Adventure'){
-    genreClass = 'border-warning';
-
-  } else if (movie.genre[0] === 'Action') {
-    genreClass = 'border-danger';
-
-  } else {
-    genreClass = 'border-dark';
-  }
-
+  var genreClass = getGenreColour(movie.genre[0]);
+  // if (movie.genre[0] === 'Animation') {
+  //   genreClass = 'border-primary';
+  //
+  // } else if (movie.genre[0] === 'Adventure'){
+  //   genreClass = 'border-warning';
+  //
+  // } else if (movie.genre[0] === 'Action') {
+  //   genreClass = 'border-danger';
+  //
+  // } else {
+  //   genreClass = 'border-dark';
+  // }
 
   var movieCard = '<div class="col-12 col-sm-6 col-md-4 mb-3 text-center">';
-    // movieCard += '<div class="movieThumb card '+ genreClass +'" onclick="showMoreMovies()">';
-      movieCard += '<div class="movieThumb movieThumb2 card '+ genreClass +' " data-id="' + movie.id +'">';
+    // movieCard += '<div class="movieThumb card h-100'+ genreClass +'" onclick="showMoreMovies()">';
+      movieCard += '<div class="movieThumb movieThumb2 card h-100 border-'+ genreClass +' " data-id="' + movie.id +'">';
       movieCard += '<img src="images/posters/' + movie.poster + '"class="card-img-top" alt="">';
       movieCard += '<div class="card-body">';
       movieCard += '<h5 class="card-title">' + movie.title + "</h5>";
@@ -141,7 +162,6 @@ for (var i = 0; i < movies.length; i++) {
   moviesList.innerHTML += movieCard;
 
 }
-
 
 function showMoreMovie(movieNumber){
   var singleMovie;
@@ -160,19 +180,37 @@ function showMoreMovie(movieNumber){
     console.log(singleMovie);
     document.getElementById('posterImage').src = 'images/posters/' + singleMovie.poster;
     document.getElementById('movieTitle').innerText = singleMovie.title;
+    document.getElementById('movieYear').innerText = singleMovie.year;
+    for (var i = 0; i < singleMovie.directors.length; i++) {
+      console.log(singleMovie.directors[i]);
+      document.getElementById('movieDirectors').innerHTML += '<li class="list-inline-item">' + singleMovie.directors[i] + '</li>';
+    }
 
+    document.getElementById('movieBio').innerText = singleMovie.bio;
+    document.getElementById('movieLength').innerText = singleMovie.movieLength;
 
+    document.getElementById('movieGenre').innerHTML = '';
+    var genreColour;
 
+    for (var i = 0; i < singleMovie.genre.length; i++) {
+      // singleMovie.genre[i]
+      if(singleMovie.genre[i] === 'Animation') {
+        genreColour = 'badge-primary';
+      } else if (singleMovie.genre[i] === 'Action') {
+        genreColour = 'badge-secondary';
+      } else if (singleMovie.genre[i] === 'Adventure') {
+        genreColour = 'badge-warning';
+      } else {
+        genreColour = 'badge-dark';
+      }
+
+      document.getElementById('movieGenre').innerHTML += '<span class="badge '+genreColour+' mr-1">' + singleMovie.genre[i] + '</span>';
+    }
 
 
     document.getElementById('moviePopUp').style.display = 'flex';
     document.body.style.overflow = 'hidden';
 }
-
-
-
-
-
 
 var movieThumbnails = document.getElementsByClassName('movieThumb2');
 for (var i = 0; i < movieThumbnails.length; i++) {
@@ -195,4 +233,18 @@ for (var i = 0; i < movieThumbnails.length; i++) {
 document.getElementById('close').onclick = function(){
     document.getElementById('moviePopUp').style.display = 'none';
     document.body.style.overflow = 'scroll';
+}
+
+function getGenreColour(genre) {
+  if (genre === 'Action') {
+    return 'primary';
+  } else if (genre === 'Adventure') {
+    return 'secondary';
+  } else if (genre === 'Animation') {
+    return 'danger';
+  } else if (genre === 'Family') {
+    return 'warning';
+  } else {
+    return 'dark';
+  }
 }
